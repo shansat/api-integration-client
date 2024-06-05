0aea726a3c07de477f578d6f5e864f88320f2639
shansat <shansat47@gmail.com>
2024-06-05 11:49:16 +0530

    MUI added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AppBar, Toolbar, Typography, Container, Box, CircularProgress } from '@mui/material';
 import './App.css';
-import {useState, useEffect} from 'react';
-import axios from 'axios'
 
 function App() {
-
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-    try {
-      const response = await axios.get("http://localhost:4000/api/message");
-      console.log(response);
-      setMessage(response.data.message);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      try {
+        const response = await axios.get('http://localhost:4000/api/message');
+        setMessage(response.data.message);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
 
-  fetchData();
-  }, [])
-
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <p>{message}</p>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My React App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Box my={4}>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <Typography variant="h4" component="p">
+              {message}
+            </Typography>
+          )}
+        </Box>
+      </Container>
     </div>
   );
 }
